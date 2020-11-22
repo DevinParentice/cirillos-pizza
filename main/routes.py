e9f8e0968fa2b12518f6f902e5ffe62d577218c8
DevinParentice <dude114100@gmail.com>
2020-11-22 00:11:39 -0500

    Added menu page

--- main/routes.py
+++ main/routes.py
@@ -7,3 +7,8 @@ main = Blueprint('main', __name__)
 @main.route('/home')
 def home():
     return render_template('home.html')
+
+
+@main.route('/menu')
+def menu():
+    return render_template('menu.html', menu_request=True)
